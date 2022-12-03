e313d6a3fc51c70f2c03433f54691ec21247b4f6
sboy99 <shadowboy191@gmail.com>
2022-12-04 00:12:17 +0530

    Added folder structure & new app wrapper

---
+++ src/Wrappers/AppWrapper.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {
+  children: JSX.Element;
+};
+
+const AppWrapper: React.FunctionComponent<Props> = ({ children }) => {
+  //
+  return children;
+};
+
+export default AppWrapper;
