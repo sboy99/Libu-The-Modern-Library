ece938df7cc31572b01b171ad5532bfef198b728
sboy99 <shadowboy191@gmail.com>
2022-12-07 00:14:34 +0530

    Added a window size tractker on theme state

--- src/app/Wrappers/AppWrapper.tsx
+++ src/app/Wrappers/AppWrapper.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useTheme } from "../store";
+import { Actions } from "../store/features";
 
 type Props = {
   children: JSX.Element;
 };
 
 const AppWrapper: React.FunctionComponent<Props> = ({ children }) => {
-  //
   const { darkMode } = useTheme();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    function handleResize() {
+      dispatch(Actions.setWindowWidth(Number(window.innerWidth)));
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return <div className={`relative ${darkMode && `dark`}`}>{children}</div>;
 };
 
