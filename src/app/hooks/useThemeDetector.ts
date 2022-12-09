199a6a44f4b79f324831c5c333a7614277a20884
sboy99 <shadowboy191@gmail.com>
2022-12-09 18:20:35 +0530

    Theme channging issue fixed

--- src/app/hooks/useThemeDetector.ts
+++ src/app/hooks/useThemeDetector.ts
@@ -1,12 +1,23 @@
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useTheme } from "../store";
+import { Actions } from "../store/features";
 
 export const useThemeDetector = (): boolean => {
+  const { isSyncWithSystem } = useTheme();
+  const dispatch = useDispatch();
   const getCurrentTheme = () =>
     window.matchMedia("(prefers-color-scheme: dark)").matches;
   const [isDarkMode, setIsDarkMode] = useState(getCurrentTheme());
 
   const mediaQueryListener = (e: MediaQueryListEvent) => {
-    setIsDarkMode(e.matches);
+    const darkMode = e.matches;
+    if (isSyncWithSystem) {
+      const themeCode = darkMode ? `defaultDark` : `defaultLight`;
+      const themeName = darkMode ? `Night Mode` : `day Mode`;
+      dispatch(Actions.setTheme({ code: themeCode, title: themeName }));
+    }
+    setIsDarkMode(darkMode);
   };
 
   useEffect(() => {
