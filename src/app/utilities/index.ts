c974757f3953bc0c0a5be88bf87cf7ddc13e25a7
sboy99 <shadowboy191@gmail.com>
2022-12-09 15:43:01 +0530

    Added typography to themes
    ++ Features
    - Now don't have to desisn typography again and again
    - Headless ui design
    
    +- Changes
    - Modified tailwind config
    - Added typography component

--- src/app/utilities/index.ts
+++ src/app/utilities/index.ts
@@ -2,5 +2,12 @@ import Component from "./Component";
 import Container from "./Container";
 import Pop from "./Pop";
 import RoundedButtons from "./RoundedButtons";
+import TypoGrapher from "./TypoGrapher";
 
-export const utilities = { Container, Component, Pop, RoundedButtons };
+export const utilities = {
+  Container,
+  Component,
+  Pop,
+  RoundedButtons,
+  TypoGrapher,
+};
