2362be8a3bb0c9aa09db53ef40dcebe6fbd46995
sboy99 <shadowboy191@gmail.com>
2022-12-12 13:19:26 +0530

    Bug-Fix: Fixed theme detector hook and refactored

--- src/app/hooks/useThemeDetector.ts
+++ src/app/hooks/useThemeDetector.ts
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { useTheme } from "../store";
 import { Actions } from "../store/features";
 import { validThemes, validNames } from "../data/Theme";
 
 export const useThemeDetector = (): boolean => {
-  const { isSyncWithSystem } = useTheme();
   const dispatch = useDispatch();
 
   const getManualThemeCode = (): string | null =>
@@ -13,35 +11,31 @@ export const useThemeDetector = (): boolean => {
   const getManualThemeName = (): string | null =>
     window.localStorage.getItem("themeName");
 
-  useEffect(() => {
-    if (getManualThemeCode()) {
-      dispatch(Actions.diableSyncWithSystem());
-
-      let currThemeCode = getManualThemeCode() ?? `defaultLight`;
-      currThemeCode = validThemes.includes(currThemeCode)
-        ? currThemeCode
-        : `defaultLight`;
-
-      let currThemeName = getManualThemeName() ?? `Default Light`;
-      currThemeName = validNames.includes(currThemeName)
-        ? currThemeName
-        : `Default Light`;
-
-      dispatch(Actions.setTheme({ code: currThemeCode, title: currThemeName }));
-    }
-  }, []);
-
   const getCurrentSystemTheme = () =>
     window.matchMedia("(prefers-color-scheme: dark)").matches;
   const [isDarkMode, setIsDarkMode] = useState(getCurrentSystemTheme());
 
+  const setTraditionalTheme = () => {
+    let themeCode = getManualThemeCode() ?? `defaultLight`;
+    themeCode = validThemes.includes(themeCode) ? themeCode : `defaultLight`;
+
+    let themeName = getManualThemeName() ?? `Default Light`;
+    themeName = validNames.includes(themeName) ? themeName : `Default Light`;
+
+    dispatch(Actions.setTheme({ code: themeCode, title: themeName }));
+  };
+
+  useEffect(() => {
+    if (getManualThemeCode()) {
+      dispatch(Actions.diableSyncWithSystem());
+      setTraditionalTheme();
+    } else {
+      dispatch(Actions.enableSyncWithSystem());
+    }
+  }, [isDarkMode]);
+
   const mediaQueryListener = (e: MediaQueryListEvent) => {
     const darkMode = e.matches;
-    if (isSyncWithSystem) {
-      const themeCode = darkMode ? `defaultDark` : `defaultLight`;
-      const themeName = darkMode ? `Night Mode` : `day Mode`;
-      dispatch(Actions.setTheme({ code: themeCode, title: themeName }));
-    }
     setIsDarkMode(darkMode);
   };
 
