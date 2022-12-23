de73c3ccb8e9fdd4264b27c8b20a8d24efc17d76
Sagar Bera <contact.sagarbera@gmail.com>
2022-12-24 01:20:18 +0530

    feat:Enabled hide signature form on click outside
    +- Changes
    - Added useOutsideClickHandler hook
    - Enabled useOutSideClickHandler hook on signature form

---
+++ src/app/hooks/useOutsideClickHandler.ts
@@ -0,0 +1,29 @@
+import { RefObject, useEffect } from "react";
+
+type Callback = () => void;
+
+function assertIsNode(e: EventTarget | null): asserts e is Node {
+  if (!e || !("nodeType" in e)) {
+    throw new Error(`Node expected`);
+  }
+}
+
+const useOutsideClickHandler = (
+  ref: RefObject<HTMLDivElement>,
+  fn: Callback
+): void => {
+  useEffect(() => {
+    function handleClickOutside({ target }: MouseEvent) {
+      assertIsNode(target);
+      if (ref.current && !ref.current.contains(target)) {
+        fn();
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside, true);
+    };
+  }, [ref]);
+};
+
+export default useOutsideClickHandler;
