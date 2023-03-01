9a96e5bd8240d4c065e5abc00610da6ff952b2bc
Sagar Bera <contact.sagarbera@gmail.com>
2023-03-01 23:08:15 +0530

    feat: defined interfaces of different component

---
+++ src/app/store/interfaces/StoreInterface.ts
@@ -0,0 +1,48 @@
+type TSignatureOptionType = 'login' | 'register';
+type TUserRole = 'reader' | 'librarian' | 'owner;';
+type TApiResponseType = 'error' | 'warning' | 'success';
+
+export interface IApiResponse<T = unknown> {
+  type: TApiResponseType;
+  message: string;
+  data?: T | T[];
+}
+
+export interface IApiState {
+  isLoading: boolean;
+  isFormLoading: boolean;
+  response: {
+    type: TApiResponseType | null;
+    message: string | null;
+  };
+  errors?: Record<string, string>;
+}
+
+export interface ILayoutState {
+  isFallingBack: boolean;
+  isSearchBarOpen: boolean;
+  isMenuOpen: boolean;
+  isSignatureFormOpen: boolean;
+  signatureOption: TSignatureOptionType;
+}
+
+export interface IThemeState {
+  windowWidth: number;
+  theme: string;
+  title: string;
+  isSyncWithSystem: boolean;
+}
+
+export interface IUser {
+  user: {
+    userId: string;
+    username: string;
+    role: TUserRole;
+  } | null;
+  profile: {
+    name: string;
+    bio: string;
+    photo: string;
+    isVerified: boolean;
+  } | null;
+}
