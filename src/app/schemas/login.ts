af11dce1d69897842e7896a60589a8d4a0857285
Sagar Bera <contact.sagarbera@gmail.com>
2023-03-01 23:10:39 +0530

    feat: added form validations

--- src/app/schemas/login.ts
+++ src/app/schemas/login.ts
@@ -1,11 +1,13 @@
-import * as yup from "yup";
+import * as yup from 'yup';
 
 const loginSchema = yup.object({
   email: yup
     .string()
+    .strict(true)
     .required(`Email is required`)
     .email(`Provide a valid email`),
   password: yup.string().required(`Password is required`),
+  rememberMe: yup.boolean().default(false),
 });
 
 export default loginSchema;
