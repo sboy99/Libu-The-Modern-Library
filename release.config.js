a2af5faf5ee1c04b71c0abe312067db334ace210
Sagar Bera <contact.sagarbera@gmail.com>
2023-03-07 01:28:07 +0530

    ci: config for semantic release

---
+++ release.config.js
@@ -0,0 +1,17 @@
+const config = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
+
+module.exports = config;
